80d249df987c97c7e55dc70c664f34d19d814d2d
Eugene Romanoff <evromanoff@gmail.com>
2021-03-18 23:25:03 +0700

    Added function 'send_mail'

--- blog/models.py
+++ blog/models.py
@@ -6,8 +6,7 @@ from django.urls import reverse
 
 class PublishedManager(models.Manager): 
     def get_queryset(self): 
-        #return super(PublishedManager, self).get_queryset().filter(status='published')
-        return super().get_queryset().filter(status='published')
+        return super(PublishedManager, self).get_queryset().filter(status='published')
 
 
 class Post(models.Model): 
@@ -28,20 +27,19 @@ class Post(models.Model):
     status = models.CharField(max_length=10,  
                               choices=STATUS_CHOICES, 
                               default='draft') 
+    
     objects = models.Manager() # The default manager. 
     published = PublishedManager() # Our custom manager.
 
     class Meta: 
         ordering = ('-publish',) 
-        verbose_name = 'Статья'
-        verbose_name_plural = 'Статьи'
 
     def __str__(self): 
         return self.title
 
     def get_absolute_url(self):
         return reverse('blog:post_detail',
-                        args=[self.publish.year,
-                            self.publish.month,
-                            self.publish.day,
-                            self.slug])  
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
