aadda13f1a50177c4f81f7bf2f6c19b22f266a8b
Eugene Romanoff <evromanoff@gmail.com>
2021-03-19 12:32:29 +0700

    Added Comment subsystem in site and Admin page

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-#from .models import Comment
+from .models import Comment
 
 
 class EmailPostForm(forms.Form):
@@ -10,8 +10,7 @@ class EmailPostForm(forms.Form):
                                widget=forms.Textarea)
 
 
-""" class CommentForm(forms.ModelForm):
+class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('name', 'email', 'body')
- """
