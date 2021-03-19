53ecf81456726d27e9cacce0f96c05f7b856a0a4
Eugene Romanoff <evromanoff@gmail.com>
2021-03-19 17:29:53 +0700

    Added recommented posts list

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Count
 from django.shortcuts import render, get_object_or_404 
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
@@ -56,14 +57,20 @@ def post_detail(request, year, month, day, post):
             new_comment.save()
     else:
         comment_form = CommentForm()
+  # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+                                  .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+                                .order_by('-same_tags','-publish')[:4]
     return render(request,
-                  'blog/post/detail.html',
-                  {'post': post,
-                   'comments': comments,
-                   'new_comment': new_comment,
-                   'comment_form': comment_form})
-                  #'similar_posts': similar_posts})                                   
-
+                    'blog/post/detail.html',
+                    {'post': post,
+                    'comments': comments,
+                    'new_comment': new_comment,
+                    'comment_form': comment_form,
+                    'similar_posts': similar_posts})                                   
+    
 class PostListView(ListView):
     queryset = Post.published.all()
     context_object_name = 'posts'
